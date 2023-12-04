59b4d90d32e86063229f7604820d21afa05dbb60
Tihomir Dimitrov <114056948+ShadowOfDiablo@users.noreply.github.com>
2023-12-04 13:02:29 +0200

    Update boot.asm

--- Asm/src/bootloader/boot.asm
+++ Asm/src/bootloader/boot.asm
@@ -30,7 +30,7 @@ ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, use
                             db 0                    ; reserved
 ebr_signature:              db 29h
 ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
-ebr_volume_label:           db 'NANOBYTE OS'        ; 11 bytes, padded with spaces
+ebr_volume_label:           db 'PopovOS    '        ; 11 bytes, padded with spaces
 ebr_system_id:              db 'FAT12   '           ; 8 bytes
 
 ;
